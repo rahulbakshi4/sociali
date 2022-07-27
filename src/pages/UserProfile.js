c66907bf08d65ec8a8d8583cebb3aa681b7e5ea0
Rahul Bakshi <rahulbakshi3@gmail.com>
2022-07-27 18:29:20 +0530

    fix: like icon on bookmark tab

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -4,14 +4,18 @@ import { useDispatch, useSelector } from "react-redux"
 import { useLocation, useParams, useNavigate } from "react-router-dom"
 import { BookmarkIcon, ProfileDetailsModal, ListIcon, LogoutIcon, Modal, Navbar, PostContainer, EditProfileModal, Loader, PostModal } from "../components"
 import { logout } from "../features/auth/authSlice"
-import { getAllBookmarks } from "../features/posts/postSlice"
-import { getUser, getUserPost, follow, unfollow } from "../features/users/usersSlice"
+import { getAllBookmarks, getAllPosts } from "../features/posts/postSlice"
+import { getUser, follow, unfollow } from "../features/users/usersSlice"
 import { openModal } from "../features/utilities/utilitySlice"
 import { isFollowing } from "../utilities/isFollowing"
 export const UserProfile = () => {
     const { user, token } = useSelector((store) => store.auth)
-    const { userProfile, userPosts } = useSelector((store) => store.users)
+    const { userProfile, profileLoading } = useSelector((store) => store.users)
     const { modalState, openPostModal } = useSelector((store) => store.utilities)
+    const { allPosts, bookmarkedPosts } = useSelector((store) => store.posts)
+    const userPosts = allPosts?.filter((post) => post.username === userProfile?.username)
+    const bookmarks = allPosts.filter((el) =>
+        userProfile?.bookmarks.some((bookmark) => bookmark._id === el._id))
     const location = useLocation()
     const navigate = useNavigate()
     const dispatch = useDispatch()
@@ -23,16 +27,16 @@ export const UserProfile = () => {
     })
     const [children, setChildren] = useState(null)
     useEffect(() => {
+        dispatch(getAllPosts())
+    }, [])
+
+    useEffect(() => {
         dispatch(getUser(userID))
-    }, [userID, userProfile?.bookmarks])
+    }, [userID, bookmarkedPosts])
 
     useEffect(() => {
         dispatch(getAllBookmarks())
     }, [userProfile?.bookmarks])
-
-    useEffect(() => {
-        dispatch(getUserPost(username))
-    }, [userProfile])
 
     const followHandler = () => {
         dispatch(follow({ token, userID: userProfile?._id }))
@@ -53,12 +57,12 @@ export const UserProfile = () => {
             <Navbar />
             <div className="">
                 <div className="bg-white max-w-3xl lg:mt-20 md:mt-20 border-2 border-gray-800 mx-auto  rounded-lg">
-                    <div className="flex p-4 items-center">
-                        <div className="flex lg:px-6 md:px-6 px-4 lg:basis-5/12">
-                            <img className="rounded-full w-44 h-44 object-cover" src={userProfile?.avatarUrl} alt="user avatar" />
+                    <div className="flex p-4 flex-col sm:flex-row">
+                        <div className="flex lg:px-6 md:px-6 px-4 lg:basis-5/12 justify-center">
+                            <img className="rounded-full w-44 h-44 object-cover " src={userProfile?.avatarUrl} alt="user avatar" />
                         </div>
 
-                        <div className="flex-grow flex flex-col gap-6 p-4">
+                        <div className="flex-grow flex flex-col gap-3 lg:gap-6 md:gap-6 p-2 lg:p-4 md:p-4">
                             <div className="flex justify-between gap-4">
                                 <h2 className="text-3xl font-light">{userProfile?.username}</h2>
                                 {userID === user?._id ? (<div className="flex gap-6 items-center">
@@ -105,7 +109,7 @@ export const UserProfile = () => {
                     </ul>)}
                     <div className="pt-1">
                         {active.posts && (userPosts?.length !== 0 ? (userPosts.map((post) => <PostContainer key={post.id} {...post} />)) : (<p className="p-4 text-center text-xl">No posts found</p>))}
-                        {active.bookmarks && (userProfile?.bookmarks.length !== 0 ? (userProfile?.bookmarks.map((post) => <PostContainer key={post.id} {...post} />)) : (<p className="p-4 text-center text-xl">No bookmarks found</p>))}
+                        {active.bookmarks && (bookmarks.length !== 0 ? (bookmarks.map((post) => <PostContainer key={post.id} {...post} />)) : (<p className="p-4 text-center text-xl">No bookmarks found</p>))}
                     </div>
                     <Modal state={modalState} children={openPostModal ? <PostModal /> : children} />
                 </div>
